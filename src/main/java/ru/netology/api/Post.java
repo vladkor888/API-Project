6d465d26055f1fdda1ae69bc9b8bc01682d32ac9
vladkor888 <lidnn97_1997@mail.ru>
2022-06-04 18:09:45 +0300

    First commit

---
+++ src/main/java/ru/netology/api/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.api;
+
+public class Post {
+
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+    public FormDate birthday;
+}
